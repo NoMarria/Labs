41c2031aed4ec3b9ada6bd8833bc93c95d2ba485
Maria <marialanchava@yandex.ru>
2026-05-20 13:23:57 +0300

    Add

---
+++ dev_to_stg.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+git checkout stg
+git merge dev --no-ff -m "Merge dev into stg $(date +'%Y-%m-%d %H:%M:%S')"
+TAG="stg-$(date +'%Y%m%d-%H%M%S')"
+git tag "$TAG"
+git push origin stg --tags
+git checkout dev
