183fd61e06c74d4de2c2f912958f9ddea54619a5
NoMarria <marialanchava@yandex.ru>
2026-05-21 00:23:33 +0300

    Update dev_to_stg.sh

--- dev_to_stg.sh
+++ dev_to_stg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 git checkout stg
-git merge dev --no-ff -m "Merge dev into stg $(date +'%Y-%m-%d %H:%M:%S')"
+git merge dev --no-ff -m "$(date +'%Y-%m-%d %H:%M:%S')"
 TAG="stg-$(date +'%Y%m%d-%H%M%S')"
 git tag "$TAG"
 git push origin stg --tags
